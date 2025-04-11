844fc8e3ff653345b26d817d646bfd1537a68085
Alondra <alondra_caflo@hotmail.com>
2025-04-10 20:27:08 -0600

    hola

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { TaskItemComponent } from "./task-item/task-item.component";
 import { TaskListComponent } from "./task-list/task-list.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [TaskItemComponent, TaskListComponent],
+  imports: [TaskItemComponent, TaskListComponent, FormsModule],
 })
 export class AppComponent {
   title = 'angular-testing-project';
